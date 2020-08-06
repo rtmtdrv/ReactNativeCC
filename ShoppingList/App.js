7ebc828e92ac4a36c351246835019f63e7d18c97
Art Todorov <art.todorov@me.com>
2020-08-05 19:49:54 -0700

    back to where I ended, switched react-native version, added a function for a random id generator

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
+  function guidGenerator() {
+    var S4 = function() {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+  }
+
+  const [items, setItems] = useState([
+    {id: guidGenerator(), text: 'Milk'},
+    {id: guidGenerator(), text: 'Eggs'},
+    {id: guidGenerator(), text: 'Bread'},
+    {id: guidGenerator(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
