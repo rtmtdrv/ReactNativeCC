f87d7e4f0f5d878ceb7fbb350cae6f95d5323621
Art Todorov <art.todorov@me.com>
2020-08-05 21:38:41 -0700

    add item functionality, delete item, an well as an error if no text

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
   function guidGenerator() {
     var S4 = function() {
+      // eslint-disable-next-line no-bitwise
       return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    return (
+      S4() +
+      S4() +
+      '-' +
+      S4() +
+      '-' +
+      S4() +
+      '-' +
+      S4() +
+      '-' +
+      S4() +
+      S4() +
+      S4()
+    );
   }
 
   const [items, setItems] = useState([
@@ -18,12 +33,31 @@ const App = () => {
     {id: guidGenerator(), text: 'Juice'},
   ]);
 
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', {text: 'ok'});
+    } else {
+      setItems(prevItems => {
+        return [{id: guidGenerator(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem}/>
       <FlatList
         data={items}
-        renderItem={({item}) => <ListItem item={item}/>}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
